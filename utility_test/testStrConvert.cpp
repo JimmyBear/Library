c5f07f464e46c895ca589387b599abae1d6cf175
jimmykuo <jimmy760205@gmail.com>
2015-05-25 21:23:03 +0800

    Add the unit-test for testing utility

---
+++ utility_test/testStrConvert.cpp
@@ -0,0 +1,31 @@
+
+#include "gtest/gtest.h"
+#include "StrConvert.h"
+ 
+using namespace utility;
+
+
+//------------------------------------------------------------------------------------------
+
+TEST(utility_test, utility_test)
+{
+	std::string str1;
+	str1 << L"WStr1";
+	ASSERT_STREQ(str1.c_str(), "WStr1");
+
+	std::string str2;
+	L"WStr2" >> str2;
+	ASSERT_STREQ(str2.c_str(), "WStr2");
+
+	std::string	str3;
+	std::wstring wstr1;
+	wstr1 << "Str1";
+	str3 << wstr1;
+	ASSERT_STREQ(str3.c_str(), "Str1");
+
+	std::string	str4;
+	std::wstring wstr2;
+	"Str2" >> wstr2;
+	wstr2 >> str4;
+	ASSERT_STREQ(str4.c_str(), "Str2");
+}
