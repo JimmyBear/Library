8af57d8104ce90de9c4e5468b13828bd33102641
jimmykuo <jimmy760205@gmail.com>
2015-05-27 23:56:35 +0800

    Adjust the testing case of WStrToStr

--- utility_test/testStrConvert.cpp
+++ utility_test/testStrConvert.cpp
@@ -7,28 +7,26 @@ using namespace utility;
 
 //------------------------------------------------------------------------------------------
 
+// The test case of converting wide string to string
 TEST(StrConvertTest, StrToWStr)
 {
 	std::string str1;
 	str1 << L"WStr1";
-	ASSERT_STREQ(str1.c_str(), "WStr1");
+	EXPECT_STREQ(str1.c_str(), "WStr1");
 
 	std::string str2;
 	L"WStr2" >> str2;
-	ASSERT_STREQ(str2.c_str(), "WStr2");
+	EXPECT_STREQ(str2.c_str(), "WStr2");
 }
 
+// The test case of converting string to wide string
 TEST(StrConvertTest, WStrToStr)
 {
-	std::string	str3;
 	std::wstring wstr1;
 	wstr1 << "Str1";
-	str3 << wstr1;
-	ASSERT_STREQ(str3.c_str(), "Str1");
+	EXPECT_STREQ(wstr1.c_str(), L"Str1");
 
-	std::string	str4;
 	std::wstring wstr2;
 	"Str2" >> wstr2;
-	wstr2 >> str4;
-	ASSERT_STREQ(str4.c_str(), "Str2");
+	EXPECT_STREQ(wstr2.c_str(), L"Str2");
 }
